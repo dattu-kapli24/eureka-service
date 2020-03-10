f5ee48de6421eb7c7c7178110841fe5bc8b15459
dattu-kapli24 <dattukapli@gmail.com>
2020-03-10 20:25:49 +0530

    initial commit

---
+++ src/test/java/com/techjava/stock/eurekhaserver/EurekhaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techjava.stock.eurekhaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
